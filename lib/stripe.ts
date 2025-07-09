5ce8373ae5ed96c9b1b71ca7e17e39b2dfc5cd91
Tobias Lüger <44581179+TobiasLueger@users.noreply.github.com>
2025-07-09 12:38:09 +0200

    Updated stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,9 +1,9 @@
 import { loadStripe } from '@stripe/stripe-js';
 import Stripe from 'stripe';
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2024-06-20',
 });
 
